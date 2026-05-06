6f1d8e0cc39327151e4b481089f9dd88b61141e8
leains <leonardusbrain0@gmail.com>
2026-05-06 21:46:44 +0700

    feat: melengkapi parse statement

--- src/syntax-analysis/ParserStatement.cpp
+++ src/syntax-analysis/ParserStatement.cpp
@@ -1,38 +1,185 @@
 #include "parser.hpp"
 
-// [TODO!] Belum ada implementasi untuk 10 fungsi di bawah!
+namespace {
+    bool isStatementFollow(TokenType type){
+        return type == TokenType::SEMICOLON ||
+               type == TokenType::ENDSY ||
+               type == TokenType::ELSESY ||
+               type == TokenType::UNTILSY ||
+               type == TokenType::PERIOD;
+    }
+
+    bool isConstantStart(TokenType type){
+        return type == TokenType::CHARCON ||
+               type == TokenType::STRING ||
+               type == TokenType::PLUS ||
+               type == TokenType::MINUS ||
+               type == TokenType::IDENT ||
+               type == TokenType::INTCON ||
+               type == TokenType::REALCON;
+    }
+}
 
 ParseNode Parser::parseCompoundStatement(){
     // beginsy + statement-list + endsy
+    ParseNode node = ParseNode("<compound-statement>");
+
+    node.children.push_back(expect(TokenType::BEGINSY));
+    node.children.push_back(parseStatementList());
+    node.children.push_back(expect(TokenType::ENDSY));
+
+    return node;
 }
 ParseNode Parser::parseStatementList(){
     // statement (semicolon + statement)*
+    ParseNode node = ParseNode("<statement-list>");
+
+    node.children.push_back(parseStatement());
+
+    while (match(TokenType::SEMICOLON)){
+        node.children.push_back(expect(TokenType::SEMICOLON));
+        node.children.push_back(parseStatement());
+    }
+
+    return node;
 }
 ParseNode Parser::parseStatement(){
-    /* (assignment-statement | 
-    if-statement | case-statement |
-    while-statement |
-    repeat-statement | for-statement | procedure/function-call )?
-    */
+    /* (assignment-statement | if-statement | case-statement |while-statement |repeat-statement | for-statement | procedure/function-call )?*/
+    ParseNode node = ParseNode("<statement>");
+
+    if (match(TokenType::IDENT)){
+        bool assignmentStatement = false;
+        int lookahead = 1;
+        TokenType nextType = peekAhead(lookahead).type;
+
+        while (!isStatementFollow(nextType) && nextType != TokenType::ERROR){
+            if (nextType == TokenType::BECOMES){
+                assignmentStatement = true;
+                break;
+            }
+            lookahead++;
+            nextType = peekAhead(lookahead).type;
+        }
+
+        if (assignmentStatement){
+            node.children.push_back(parseAssignmentStatement());
+        } else {
+            node.children.push_back(parseProcedureFunctionCall());
+        }
+    } else if (match(TokenType::IFSY)){
+        node.children.push_back(parseIfStatement());
+    } else if (match(TokenType::CASESY)){
+        node.children.push_back(parseCaseStatement());
+    } else if (match(TokenType::WHILESY)){
+        node.children.push_back(parseWhileStatement());
+    } else if (match(TokenType::REPEATSY)){
+        node.children.push_back(parseRepeatStatement());
+    } else if (match(TokenType::FORSY)){
+        node.children.push_back(parseForStatement());
+    } else if (!isStatementFollow(peek().type)){
+        error("unexpected token " + tokenToString(peek().type) + " in statement");
+    }
+
+    return node;
 }
 ParseNode Parser::parseAssignmentStatement(){
-    // variable + becomes + expressio
+    // variable + becomes + expression
+    ParseNode node = ParseNode("<assignment-statement>");
+
+    node.children.push_back(parseVariable());
+    node.children.push_back(expect(TokenType::BECOMES));
+    node.children.push_back(parseExpression());
+
+    return node;
 }
 ParseNode Parser::parseIfStatement(){
     // ifsy + expression + thensy + statement + (elsy + statement)?
+    ParseNode node = ParseNode("<if-statement>");
+
+    node.children.push_back(expect(TokenType::IFSY));
+    node.children.push_back(parseExpression());
+    node.children.push_back(expect(TokenType::THENSY));
+    node.children.push_back(parseStatement());
+    if (match(TokenType::ELSESY)){
+        node.children.push_back(expect(TokenType::ELSESY));
+        node.children.push_back(parseStatement());
+    }
+
+    return node;
 }
 ParseNode Parser::parseCaseStatement(){
     // casesy + expression + ofsy + case-block + endsy
+    ParseNode node = ParseNode("<case-statement>");
+
+    node.children.push_back(expect(TokenType::CASESY));
+    node.children.push_back(parseExpression());
+    node.children.push_back(expect(TokenType::OFSY));
+    node.children.push_back(parseCaseBlock());
+    node.children.push_back(expect(TokenType::ENDSY));
+
+    return node;
 }
 ParseNode Parser::parseCaseBlock(){
     // constant + (comma + constant)* + colon + statement +  (semicolon + case-block?)* 
+    ParseNode node = ParseNode("<case-block>");
+
+    node.children.push_back(parseConstant());
+    while (match(TokenType::COMMA)){
+        node.children.push_back(expect(TokenType::COMMA));
+        node.children.push_back(parseConstant());
+    }
+    node.children.push_back(expect(TokenType::COLON));
+    node.children.push_back(parseStatement());
+
+    while (match(TokenType::SEMICOLON)){
+        node.children.push_back(expect(TokenType::SEMICOLON));
+        if (isConstantStart(peek().type)){
+            node.children.push_back(parseCaseBlock());
+        }
+    }
+
+    return node;
 }
 ParseNode Parser::parseWhileStatement(){
     // whilesy + expression + dosy + statement
+    ParseNode node = ParseNode("<while-statement>");
+
+    node.children.push_back(expect(TokenType::WHILESY));
+    node.children.push_back(parseExpression());
+    node.children.push_back(expect(TokenType::DOSY));
+    node.children.push_back(parseStatement());
+
+    return node;
 }
 ParseNode Parser::parseRepeatStatement(){
     // repeatsy + statement-list + untilsy + expression
+    ParseNode node = ParseNode("<repeat-statement>");
+
+    node.children.push_back(expect(TokenType::REPEATSY));
+    node.children.push_back(parseStatementList());
+    node.children.push_back(expect(TokenType::UNTILSY));
+    node.children.push_back(parseExpression());
+
+    return node;
 }
 ParseNode Parser::parseForStatement(){
     // forsy + ident + becomes + expression + ( tosy | downtosy) + expression + dosy + statement
+    ParseNode node = ParseNode("<for-statement>");
+
+    node.children.push_back(expect(TokenType::FORSY));
+    node.children.push_back(expect(TokenType::IDENT));
+    node.children.push_back(expect(TokenType::BECOMES));
+    node.children.push_back(parseExpression());
+    if (match(TokenType::TOSY)){
+        node.children.push_back(expect(TokenType::TOSY));
+    } else if (match(TokenType::DOWNTOSY)){
+        node.children.push_back(expect(TokenType::DOWNTOSY));
+    } else {
+        error("unexpected token " + tokenToString(peek().type) + ", expected tosy or downtosy");
+    }
+    node.children.push_back(parseExpression());
+    node.children.push_back(expect(TokenType::DOSY));
+    node.children.push_back(parseStatement());
+
+    return node;
 }
